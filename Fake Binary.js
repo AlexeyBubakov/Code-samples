fed7048ef96675df96a9e29ea9f9d23bc03c7124
AlexeyBubakov <100151075+AlexeyBubakov@users.noreply.github.com>
2022-04-02 17:47:55 +0300

    Update Fake Binary.js

--- Fake Binary.js
+++ Fake Binary.js
@@ -1,6 +1,8 @@
+// # Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
 function fakeBin(x) {
     let array = x.split("").map(element => element < 5 ? 0 : 1).join("");
     return array
 }
 
-// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
